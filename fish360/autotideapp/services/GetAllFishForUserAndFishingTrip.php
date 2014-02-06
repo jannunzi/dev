c9540c9c985915eccd7c2d833b8fcf599ff8697d
jannunzi <jannunzi@gmail.com>
2014-02-05 20:23:21 -0500

    Fixed mapping between trips and fish

--- fish360/autotideapp/services/GetAllFishForUserAndFishingTrip.php
+++ fish360/autotideapp/services/GetAllFishForUserAndFishingTrip.php
@@ -24,10 +24,13 @@
 
 	connectToDB();
 
-	$select = "SELECT name,`node`.title, `content_type_fish_type`.nid  ";
-	$from = "FROM `node`, `users`, `content_type_fish_type` ";
-	$where = "WHERE `node`.type = 'fish_type' AND `users`.name='$username' AND `node`.uid=`users`.uid AND `content_type_fish_type`.field_fishing_trip_nid = $fishingTripNodeId AND `node`.nid = `content_type_fish_type`.nid";
-	$query = $select.$from.$where;
+    $select = "select nf.*, f.*";
+    $from   = "from node nt, node nf, content_type_fishing_trip_type t, content_type_fish_type f";
+    $where  = "where nt.nid = t.nid AND nf.nid = f.nid AND f.field_fishing_trip_nid = t.nid AND nt.nid = $fishingTripNodeId";
+//	$select = "SELECT name,`node`.title, `content_type_fish_type`.nid  ";
+//	$from = "FROM `node`, `users`, `content_type_fish_type` ";
+//	$where = "WHERE `node`.type = 'fish_type' AND `users`.name='$username' AND `node`.uid=`users`.uid AND `content_type_fish_type`.field_fishing_trip_nid = $fishingTripNodeId AND `node`.nid = `content_type_fish_type`.nid";
+//	$query = $select.$from.$where;
 	
 	$results = mysql_query($query);
 	$num_rows = mysql_num_rows($results);
