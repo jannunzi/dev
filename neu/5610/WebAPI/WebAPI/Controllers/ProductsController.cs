723f95bb2271bb823a640871e9fa0b7c907cd309
jannunzi <jannunzi@gmail.com>
2014-03-20 22:52:40 -0400

    Web API Example

---
+++ neu/5610/WebAPI/WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ProductsApp.Models;
+
+namespace ProductsApp.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        Product[] products = new Product[] 
+        { 
+            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 }, 
+            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M }, 
+            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M } 
+        };
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        public string PutProduct(int id, Product product)
+        {
+            return "put " + product.Name;
+        }
+
+//        public HttpResponseMessage PostProduct(Product item)
+        public string PostProduct(Product product)
+        {
+            return "post " + product.Name;
+        }
+
+        public IEnumerable<Product> GetProductsByCategory(string category)
+        {
+            Product[] products = new Product[] 
+            { 
+                new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 }, 
+                new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M }, 
+                new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M } 
+            }; 
+            return products;
+        }
+
+        public string DeleteProduct(int id)
+        {
+            return "delete " + id;
+        }
+    }
+}
---
+++ neu/5610/WebAPI/WebAPI/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProductsApp.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+        public Product(int id, string name, string category, decimal price)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Category = category;
+            this.Price = price;
+        }
+        public Product() { }
+    }
+}
