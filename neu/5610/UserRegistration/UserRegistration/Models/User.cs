255d03642fb0fb7f6a4f557fe543674424a81935
jannunzi <jannunzi@gmail.com>
2014-03-20 23:35:41 -0400

    User Registration Experiment Web API Services and Client

---
+++ neu/5610/UserRegistration/UserRegistration/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UserRegistration.Models
+{
+    public class User
+    {
+        public string username { set; get; }
+        public string password { set; get; }
+    }
+}
---
+++ neu/5610/UserRegistration/UserRegistration/UsersController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using UserRegistration.Models;
+
+namespace UserRegistration
+{
+    public class UsersController : ApiController
+    {
+        public List<User> GetAllUser()
+        {
+            List<User> users = new List<User>();
+            User u1 = new User {username="user1", password="p1"};
+            User u2 = new User {username="user2", password="p2"};
+            User u3 = new User { username = "user3", password = "p3" };
+            users.Add(u1);
+            users.Add(u2);
+            users.Add(u3);
+            return users;
+        }
+        public User GetUser(int id)
+        {
+            User u = new User { username = "user4", password = "p4" };
+            return u;
+        }
+        public string PostUser(User newUser)
+        {
+            return newUser.username;
+        }
+        public string PutUser(User oldUser)
+        {
+            return oldUser.username;
+        }
+        public string DeleteUser(int id)
+        {
+            return "Delete " + id;
+        }
+    }
+}
