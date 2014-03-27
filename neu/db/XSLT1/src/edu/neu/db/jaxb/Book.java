e393fc9c18f951e325c34cc06a85cf9a3b17481c
jannunzi <jannunzi@gmail.com>
2014-03-27 16:17:35 -0400

    JAXB Examples

--- neu/db/XSLT1/src/edu/neu/db/jaxb/Book.java
+++ neu/db/XSLT1/src/edu/neu/db/jaxb/Book.java
@@ -1,5 +1,64 @@
 package edu.neu.db.jaxb;
 
+import java.util.Date;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
 public class Book {
-	
+	String title;
+	String author;
+	String genre;
+	String publisher;
+	float price;
+	Date date;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getGenre() {
+		return genre;
+	}
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+	public String getPublisher() {
+		return publisher;
+	}
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public Book(String title, String author, String genre, String publisher,
+			float price, Date date) {
+		super();
+		this.title = title;
+		this.author = author;
+		this.genre = genre;
+		this.publisher = publisher;
+		this.price = price;
+		this.date = date;
+	}
+	public Book() {
+		super();
+	}
 }
--- neu/db/XSLT1/src/edu/neu/db/jaxb/Library.java
+++ neu/db/XSLT1/src/edu/neu/db/jaxb/Library.java
@@ -1,5 +1,23 @@
 package edu.neu.db.jaxb;
 
-public class Library {
+import java.util.List;
 
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Library {
+	List<Book> book;
+	public List<Book> getBook() {
+		return book;
+	}
+	public void setBook(List<Book> books) {
+		this.book = books;
+	}
+	public Library(List<Book> books) {
+		super();
+		this.book = books;
+	}
+	public Library() {
+		super();
+	}
 }
