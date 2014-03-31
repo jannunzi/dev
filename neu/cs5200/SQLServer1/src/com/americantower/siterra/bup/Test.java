e0f9103ccb3c9049d8b9b1a8a5040f37c992aef1
jannunzi <jannunzi@gmail.com>
2014-03-31 09:36:36 -0400

    Connection to SQL Server 2000 from JDBC

---
+++ neu/cs5200/SQLServer1/src/com/americantower/siterra/bup/Test.java
@@ -0,0 +1,44 @@
+package com.americantower.siterra.bup;
+
+import java.sql.*;
+
+public class Test
+{
+	public static void main(String[] args)
+	{
+		Connection connection = null;
+		try {
+			Driver d = (Driver)Class.forName("com.microsoft.jdbc.sqlserver.SQLServerDriver").newInstance();
+			connection = DriverManager.getConnection("jdbc:microsoft:sqlserver://QCSMN01:1433;databaseName=ShareGen", "semaan_app_user", "qcdb01");
+			System.out.println(connection);
+			String testSql = "select * from ShareGenMain";
+			PreparedStatement statement = connection.prepareStatement(testSql);
+			ResultSet results = statement.executeQuery();
+			while(results.next())
+			{
+				String towerNumber = results.getString("TowerNumber");
+				String siteName = results.getString("SiteName");
+				System.out.println(towerNumber + " " + siteName);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+}
